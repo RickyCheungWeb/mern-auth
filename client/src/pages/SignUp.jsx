95a3beb3d613dfbe562ca8fdd1ae35b13e16cb00
Ricky <rickycreact@gmail.com>
2024-02-03 22:34:30 +1100

    Complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,31 +1,72 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function SignUp() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setError(false);
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await res.json();
+      setLoading(false);
+      setError(false);
+
+      if (!res.ok) {
+        throw new Error();
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Username'
           id='username'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='Email'
           id='email'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='Password'
           id='password'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
-        <button className='bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80'>
-          Sign up
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign up'}
         </button>
       </form>
 
@@ -35,8 +76,7 @@ function SignUp() {
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-700 mt-5'> Something went wrong</p>}
     </div>
   );
 }
-
-export default SignUp;
