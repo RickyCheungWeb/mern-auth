79fb4a6584bdc5f8b8bf211f7c3bd27dd297f738
Ricky <rickycreact@gmail.com>
2024-02-05 14:21:35 +1100

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <p>About</p>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text3xl font-bold mb-4 text-size-slate-700'>About</h1>
+      <p className='mb-4 text-slate-700'>
+        Hello, I'm Ricky, an aspiring developer with a profound passion for
+        coding and application development. I've been actively engaged in
+        expanding my knowledge of React through online courses, gaining
+        proficiency in React hooks, Redux, and React Router. Additionally, I'm
+        currently working on gaining backend experience by constructing a
+        foundational backend using NodeJS, MongoDB, and Firebase.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        In tandem with my self-learning journey, I recently completed a
+        Certificate IV in Information Technology (Web Development) at TAFE. The
+        program covered a range of topics, including web content transfer, PHP,
+        relational database query language, JavaScript, Python, HTML, CMS,
+        website accessibility, SEO, responsive website layouts, and cyber
+        security risk management.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        This application marks the inception of my personal exploration into
+        developing full-stack web applications with authentication using the
+        MERN stack. I'm enthusiastic about embarking on my career as a front-end
+        or full-stack developer. Feel free to contact me at 0420 883 450 or via
+        email at rickycreate@gmail.com.
+      </p>
     </div>
-  )
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text3xl font-bold mb-4 text-size-slate-700'>
+        Welcome to Ricky's Auth App!
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        This web application is developed using the MERN (MongoDB, Express,
+        React, Node.js) stack. It incorporates authentication functionalities
+        enabling users to register, authenticate, and logout. Additionally, it
+        grants access to restricted routes exclusively to authenticated users.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        The application's front-end is constructed with React, utilizing React
+        Router for client-side routing. The back-end is developed with Node.js
+        and Express, utilizing MongoDB as the database. Authentication is
+        implemented through the use of JSON Web Tokens (JWT).
+      </p>
+      <p className='mb-4 text-slate-700'>
+        This application is designed as a starting point for my personal journey
+        in learning to build full-stack web applications with authentication
+        using the MERN stack. I am enthusiastic about beginning my career as a
+        front-end or full-stack developer. Feel free to contact me at 0420 883
+        450 or via email at rickycreate@gmail.com.
+      </p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
